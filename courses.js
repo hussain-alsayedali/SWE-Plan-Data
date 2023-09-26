94c811cee075f6eb165b4d6777dbcbf59ffb3483
Hussain <hussain.alsayedali@gmail.com>
2023-09-26 10:37:35 +0300

    finsihed all courses

--- courses.js
+++ courses.js
@@ -14,6 +14,22 @@ class Course {
         console.log(this.Prerequisites)
     }
 }
+class Plan{
+    constructor(term1, term2, term3, term4, term5, term6, term7,summerTerm ,term9) {
+        this.firstTerm = term1;
+        this.secondTerm = term2;
+        this.thirdterm = term3;
+        this.fourthTerm = term4;
+        this.fifthTerm = term5;
+        this.sixthTerm = term6;
+        this.seventhTerm = term7;
+        this.summerTerm = summerTerm
+        this.ninthTerm = term9
+    }
+    printPrequisistes(){
+        console.log(this.Prerequisites)
+    }
+}
 
 let math101 = new Course("Math 101" , 4 , "Math Department" , true , false , null ,null , null)
 let math102 = new Course("Math 102" , 4 , "Math Department" , true , false , [math101] ,null , null)
@@ -58,11 +74,27 @@ let coe233 = new Course("COE 233" , 3 , "Computer Engineering Department" , fals
 
 let ics108 = new Course("ICS 108" , 4 , "Information and Computer Science Department" , false , true, [ics104] ,null , null)
 let ics202 = new Course("ICS 202" , 4 , "Information and Computer Science Department" , false , true, [ics108] ,null , null)
-let icss253 = new Course("ICS 202" , 3 , "Information and Computer Science Department" , false , false, [ics104] ,null , null)
+let ics253 = new Course("ICS 202" , 3 , "Information and Computer Science Department" , false , false, [ics104] ,null , null)
 let ics321 = new Course("ICS 321" , 3 , "Information and Computer Science Department" , false , false, [ics202] ,null , null)
 let ics343 = new Course("ICS 343" , 4 , "Information and Computer Science Department" , false , true, [ics108] ,null , null)
 let ics344 = new Course("ICS 344" , 3 , "Information and Computer Science Department" , false , false, [ics343] ,null , null)
 let ics433 = new Course("ICS 433" , 3 , "Information and Computer Science Department" , false , false, [coe233] ,null , null)
+
+
+let swe206 = new Course("SWE 206" , 3 , "Software Engineering Department" , false , true, [ics108] ,null , null)
+let swe216 = new Course("SWE 216" , 3 , "Software Engineering Department" , false , false, [swe206] ,null , null)
+let swe316 = new Course("SWE 316" , 3 , "Software Engineering Department" , false , false, [swe206] ,null , null)
+let swe326 = new Course("SWE 326" , 3 , "Software Engineering Department" , false , false, [swe216] ,null , null)
+let swe387 = new Course("SWE 387" , 3 , "Software Engineering Department" , false , false, null ,"junior" , null)
+let swe363 = new Course("SWE 363" , 3 , "Software Engineering Department" , false , false, null ,"junior" , null)
+
+let swe399 =  new Course("SWE 399" , 0 , "Software Engineering Department" , false , false, [eng214, swe363] ,null , null)
+
+let swexxx = new Course("SWE XXX" , 3 , "Software Engineering Department" , false , false, null, null , null)
+let swe439 =  new Course("SWE 439" , 3 , "Software Engineering Department" , false , false, [stat319, swe316] ,null , null)
+let swe411 =  new Course("SWE 411" , 3 , "Software Engineering Department" , false , true, [swe316, swe387] ,null , null)
+let swe412 =  new Course("SWE 412" , 2 , "Software Engineering Department" , false , true, [swe411, swe326] ,null , null)
+
 
 
 math102.printPrequisistes()
